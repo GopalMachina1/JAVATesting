3c0dc199cdb4d1ae849f92550f9528f85ba7775a
madhu gowra <gopalmachina@gmail.com>
2024-02-20 18:01:20 +0530

    hi

--- day2/A.java
+++
@@ -1,9 +0,0 @@
-package com.java.day2;
-
-public class A {
-
-	
-	public void test() {
-	System.out.println("this is a parent method");
-	}
-}
--- day4/DemoConstructor.java
+++
@@ -1,8 +0,0 @@
-package com.java.day4;
-
-public class DemoConstructor{ 
-public void m1() {
-	public Demo
-}
-
-}
